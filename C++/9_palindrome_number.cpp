064b70f793f3cac83ade93879066054993d7b10b
LeeHyunji <leehyunji0715@gmail.com>
2020-12-27 23:10:19 +0900

    Finish checking the given number is palindrome or not

--- C++/9_palindrome_number.cpp
+++ C++/9_palindrome_number.cpp
@@ -1,1 +1,23 @@
-Git push
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int len = s.length();
+        
+        if(x < 0)
+            return false;
+        
+        if(len % 2 == 0){
+            for(int i=0; i<len/2; i++ )
+                if(s[i] != s[len-1-i])
+                    return false; 
+        }
+        else {
+            for(int i=0; i<len/2; i++ )
+                if(s[i] != s[len-1-i])
+                    return false; 
+        }
+
+        return true;
+    }
+};
