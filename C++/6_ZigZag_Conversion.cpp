0f93753bcae5addf89a7b38894f99d2dd4876fa3
LeeHyunji <leehyunji0715@gmail.com>
2020-12-31 00:46:47 +0900

    Using string type vector, return a string in zigzag form

--- C++/6_ZigZag_Conversion.cpp
+++ C++/6_ZigZag_Conversion.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int length = s.length();
-        int interval = numRows + (numRows-2);
-        int at = 0;
-        char result[length];
+        // ex. PAYPALISHIRING
+        // string[0]: P A H N
+        // string[1]: A P L S I I G
+        // string[2]: Y I R
         
-        for(int i=0; i<numRows; i++){
-            for(int j=0; j<length; j++){
-                if(j % interval == i){
-                    result[j] = s[at];
-                    at++;
-                }
-            }
+        vector<string> vs(numRows, "");
+        string result = "";
+        int n = s.length();
+        int at = 0;
+        
+        while (at < n) 
+        {
+            /*  at < n: prevent memory area intrusion(?) */
+            for (int j = 0; j < numRows  && at < n ; j++) 
+                vs[j].push_back(s[at++]);
+            
+            for (int j = numRows-2; j > 0 && at < n; j--)
+                vs[j].push_back(s[at++]);
         }
-        cout << interval << "-";
-        cout << result << "\n";
+        
+        for (string str: vs){
+            str.erase(remove(str.begin(), str.end(), '\x00'), str.end());
+            result += str; 
+        }
         return result;
     }
 };
